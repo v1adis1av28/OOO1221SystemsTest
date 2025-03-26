04cbe2b2f3a71b0939556e13db137c26f7ffc7ed
Vladislav Karapuzov <VladisloveKrpzv@yandex.ru>
2025-03-26 15:56:20 +0300

    create folder structure

---
+++ TestTask/src/main/java/com/test/TestTask/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.test.TestTask.repositories;
+
+import com.test.TestTask.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findUserById(int id);
+}
